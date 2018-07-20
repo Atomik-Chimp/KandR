e8e6feb76f7f1d4dbe0b651d3173c3056e98868f
Justin Breese <particle_m@hotmail.com>
2018-07-20 00:12:57 -0800

    Added Exercise 2 outline file from home

---
+++ Chapter 1/ex1_2.cpp
@@ -0,0 +1,22 @@
+/* The C Programming Language */
+/* Chapter 1 */
+/* Exercise 2 */
+/* haven't yet made this work for example 2 */
+
+/* This runs on Visual Studio 2010 */
+/* as an ugly mix of C and C++ */
+
+/* Visual Studio 2010 got angry about not including "stdafx.h" and precompiled headers */
+#include "stdafx.h"
+
+#include <stdio.h>
+
+/* Visual Studio 2010 requires a main return type */
+/* won't let me have more than one main function (not surprising) */
+int main(){
+
+	printf("Hello World!\n");
+
+	/* note there is no pause, so the console window closes right away */
+	/* But it does appear to work */
+}
