311833f5922f1b23ec3e587b9f56203024951f6e
particle-m <particle_m@hotmail.com>
2018-07-19 16:11:13 -0800

    Started working on the project

--- KandR.cpp
+++ KandR.cpp
@@ -1,5 +1,12 @@
+
+/* KandR.cpp - Code and exercises from the book "The C Programming Language" by Kernighan and Ritchie */
+/* */
+/* I'm going to try to create these exercises as separate files */
+
+
+/* This note is from Visual Studio:
 // KandR.cpp : Defines the entry point for the console application.
-//
+*/
 
 #include "stdafx.h"
 
