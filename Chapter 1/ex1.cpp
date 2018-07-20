e9e62917923510c36cbcc2ad1b9521dd82045422
particle-m <particle_m@hotmail.com>
2018-07-19 16:25:10 -0800

    Added a solution to Chapter 1 Exercise 1

---
+++ Chapter 1/ex1.cpp
@@ -0,0 +1,23 @@
+
+/* The C Programming Language */
+/* Chapter 1 */
+/* Exercise 1 */
+/* run the hello World example program on your system */
+
+/* This runs on Visual Studio 2010 */
+/* as an ugly mix of C and C++ */
+
+/* Visual Studio 2010 got angry about not including "stdafx.h" and precompiled headers */
+#include "stdafx.h"
+
+#include <stdio.h>
+
+/* Visual Studio 2010 requires a main return type */
+int main(){
+
+	printf("Hello World!\n");
+
+	/* note there is no pause, so the console window closes right away */
+	/* But it does appear to work */
+}
+
